b13b6aef951e6cf60fadc52d510529e2b9f1f3d8
Henok0728 <henokgizaw06@gmail.com>
2026-01-25 00:01:58 +0300

    finished files

--- Server_side/Server.java
+++ Server_side/Server.java
@@ -1,34 +1,35 @@
 package Server_side;   
 import java.net.*;
 import java.io.*;
-import Calculator.Calculator;
+import Implementation_side.Calculator;
+
+
+
 public class Server {
     private Socket socket;
     private ServerSocket server;
     private DataInputStream in;
     private DataOutputStream out;
-   // private BufferedReader input;
     public Server(int port){
         try{
             server = new ServerSocket(port);
+            Calculator calc = new Calculator();
             System.out.println("server started");
             System.out.println("Waiting for the client........");
 
             socket = server.accept();
             System.out.println("client accepted");
-            
+           
             out = new DataOutputStream(socket.getOutputStream());
-          //  input = new BufferedReader(new InputStreamReader(System.in));
             in = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
             String[] line = new String[2];
-            // String send = "";
             while(true){
                 try{
                     line = in.readUTF().split("\\s+");
                     int n1 = Integer.parseInt(line[0]);
                     char op = line[1].charAt(0);
                     int n2 = Integer.parseInt(line[2]);
-                    double result = calculate(n1,n2,op);
+                    Number result = calc.calculate(n1,n2,op);
                     System.out.println("client says: "+n1+" "+op+" "+n2);
                     out.writeUTF("result is: "+result);
                     out.flush();
@@ -43,9 +44,7 @@ public class Server {
             in.close();   
         }
         catch(IOException e){System.out.println(e);}
-
-        }
-        
+    }
         public static void main(String args[]){
             Server server = new Server(8080);
         }
