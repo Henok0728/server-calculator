01a3067d1445ca873515b447fdb39977d5e5bb51
Henok0728 <henokgizaw06@gmail.com>
2026-01-25 20:06:20 +0300

    ui design

--- Client_side/client_ui/UI.java
+++ Client_side/client_ui/UI.java
@@ -1,5 +1,20 @@
 package Client_side.client_ui;
+import javax.swing.*;
+import java.awt.*;
 
-public class UI {
+public class UI extends JFrame{
+    public UI(){
+    this.setTitle("Calculator Client");
+    this.setSize(400,500);
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setVisible(true);
+    }
+
     
+    
+
+    
+public static void main(String args[]){
+    new UI();
+}
 }
