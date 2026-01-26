8c0f6d13870c1e50ea192d4e6d94a578583cfc7c
Henok0728 <henokgizaw06@gmail.com>
2026-01-26 08:32:31 +0300

    tweaking server

--- Server_side/Server.java
+++ Server_side/Server.java
@@ -1,29 +1,37 @@
 package Server_side;   
 import java.net.*;
 import java.io.*;
-import Implementation_side.Calculator;
+
 
 
 
 public class Server {
     private Socket socket;
     private ServerSocket server;
-    private DataInputStream in;
-    private DataOutputStream out;
     private boolean running;
+
     
     public Server (int port){
+
+        running = true;
+        
         try{
+            while (running){
+            server = new ServerSocket(port);
             System.out.println("server started on port: "+port);
             System.out.println("Waiting for the client........");
-            server = new ServerSocket(port);
             socket = server.accept();
-            System.out.println("New client accepted");
+            System.out.println("New client connected"); 
+            new ClientHandle(socket).start();   
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
            
-            
-    }
+}
         public static void main(String args[]){
-            Server server = new Server(8080);
+            new Server(8080);
         }
     }
     
